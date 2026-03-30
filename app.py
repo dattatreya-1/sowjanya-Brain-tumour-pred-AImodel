0708266502f1778a229296c6d92b2e4a55c6f614
dattatreya-1 <157103025+dattatreya-1@users.noreply.github.com>
2026-03-30 17:14:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,13 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image", use_column_width=True)
 
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as tmp:
+        image.save(tmp.name, format="JPEG")
+
+        prediction = predict_image(tmp.name)
+
+    st.write(f"### Prediction: {prediction}")
+
     # Save temp image
     with tempfile.NamedTemporaryFile(delete=False) as tmp:
         image.save(tmp.name)
