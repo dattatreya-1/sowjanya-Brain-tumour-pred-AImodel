2d63bbf44c1e1b573385f4f1ab869d04a097b2ee
dattatreya-1 <157103025+dattatreya-1@users.noreply.github.com>
2026-03-30 17:12:30 -0400

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,31 +1,22 @@
-
-import numpy as np
 import joblib
-from tensorflow.keras.models import load_model
-from tensorflow.keras.applications import EfficientNetB0
-from tensorflow.keras import layers, Model
+import numpy as np
+from PIL import Image
 
-# Load models
-cnn_model = load_model('cnn_model.h5')
+# Load model
 lgb_model = joblib.load('lightgbm_mri_classifier.pkl')
-
-# EfficientNet feature extractor
-base_model = EfficientNetB0(weights='imagenet', include_top=False, input_shape=(224,224,3))
-x = layers.GlobalAveragePooling2D()(base_model.output)
-feature_extractor = Model(inputs=base_model.input, outputs=x)
 
 class_names = ['glioma', 'meningioma', 'notumor', 'pituitary']
 
-def predict_image(img_array):
-    # CNN prediction
-    cnn_pred = cnn_model.predict(img_array)
-    cnn_class = class_names[np.argmax(cnn_pred)]
+IMG_SIZE = 224
 
-    # Feature extraction
-    features = feature_extractor.predict(img_array)
+def preprocess_image(img_path):
+    img = Image.open(img_path).convert("RGB")
+    img = img.resize((IMG_SIZE, IMG_SIZE))
+    img = np.array(img) / 255.0
+    img = img.flatten().reshape(1, -1)
+    return img
 
-    # LightGBM prediction
-    lgb_pred = lgb_model.predict(features)
-    lgb_class = class_names[int(lgb_pred[0])]
-
-    return cnn_class, lgb_class
+def predict_image(img_path):
+    img = preprocess_image(img_path)
+    pred = lgb_model.predict(img)
+    return class_names[int(pred[0])]
